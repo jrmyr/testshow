f1ef3a8f9f3063224952d830950f9bd8c0971da2
Jochen R. Meyer <jochen.meyer@investify.lu>
2020-12-13 21:57:46 +0100

    Initial commit

---
+++ src/main/java/de/myrnet/testshow/TestshowApplication.java
@@ -0,0 +1,13 @@
+package de.myrnet.testshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestshowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/myrnet/testshow/TestshowApplicationTests.java
@@ -0,0 +1,13 @@
+package de.myrnet.testshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestshowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
